37cdaffd34d35b77cd54a00d964923ce4df56d42
JCDayauon <demonlordex700@gmail.com>
2020-12-30 16:04:59 +0900

    first commit

---
+++ LO2UC2.js
@@ -0,0 +1,21 @@
+alert("wazzup");
+
+var myAge = 29;
+
+if (myAge > 30) {
+    document.write("You are old");
+
+} 
+
+else if (myAge > 20){
+    document.write ("Get a job");
+}
+
+else if (myAge > 10){
+    document.write ("Go Study");
+}
+
+else{   
+
+    document.write ("You are too Young");
+}
